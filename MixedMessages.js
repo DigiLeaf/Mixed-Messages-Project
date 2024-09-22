db5754b2d2b94ec6dc52f32792d0bc51cff4f7df
DigiLeaf <leiforth01@hotmail.com>
2024-09-22 12:19:53 -0500

    bug fixes to determining array index

--- MixedMessages.js
+++ MixedMessages.js
@@ -10,11 +10,13 @@ const sndArray = ["future is ", "past is ", "home will be " ]
 const endArray=["not your chains", "bright", "full of good memories"]
 
 // Determining array indexes
-let lstArrInd = Math.floor(math.random()*1);
-let sndArrInd = Math.floor(math.random()*2);
-let endArrInd = Math.floor(math.random()*2);
+let lstArrInd = Math.floor(Math.random()*2);
+let sndArrInd = Math.floor(Math.random()*3);
+let endArrInd = Math.floor(Math.random()*3);
 
 //Getting the pieces of the sentence
 let begSent = begArray[lstArrInd];
 let sndSent = sndArray[sndArrInd];
 let endSent = endArray[endArrInd];
+
+console.log(begSent + sndSent + endSent)
