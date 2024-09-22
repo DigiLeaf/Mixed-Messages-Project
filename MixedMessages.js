c197c22e4e2f1f998a7c45e1dd341d1e9a7191e2
DigiLeaf <leiforth01@hotmail.com>
2024-09-22 12:02:52 -0500

    Add sentence arrays

---
+++ MixedMessages.js
@@ -0,0 +1,12 @@
+//Topic: Fortune telling
+
+//First part of the sentence
+const begArray =["Your ", "The "];
+
+//middle of the sentence
+const sndArray = ["future is ", "past is ", "home will be " ]
+
+//end of the sentence
+const endArray=["not your chains", "bright", "full of good memories"]
+
+let begSent;
